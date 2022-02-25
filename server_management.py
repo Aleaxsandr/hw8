6f6bd613283e7060299142dbeb36bddadd4442e6
Анихимовский Александр <98551398+Aleaxsandr@users.noreply.github.com>
2022-02-25 09:50:58 +0300

    Add files via upload

---
+++ server_management.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+browser = webdriver.Chrome('C:\\Users\\ADMIN\\Desktop\\chromedriver\\chromedriver_win32\\chromedriver.exe')
+browser.get("https://itproger.com/intensive/python")
